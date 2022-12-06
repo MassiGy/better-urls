5c23089006ad676c493315d43cde0710bd32fda6
MassiGy <ghernaoutmassi@gmail.com>
2022-12-06 19:34:08 +0100

    +auth0-implementation: added the sign in with google button

--- app.js
+++ app.js
@@ -10,13 +10,25 @@ const api_routes = require("./routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
+// app.use(helmet());
 app.engine('ejs', ejs_mate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/assets', express.static('assets'));
 app.use('/images', express.static('images'));
 
+// set the referer as such for developpement
+app.use((req, res, next) => {
+    req.headers.referer = " no-referrer-when-downgrade";
+    next();
+});
+
+
+// to allow popup from google apis to work on our app
+app.use((req, res, next) => {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    next();
+});
 
 
 
